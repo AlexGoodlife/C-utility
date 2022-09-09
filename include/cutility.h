53b8ffee1725974a5185a4fa9b65111eb8208097
Alexandre Boavida <a68781@ualg.pt>
2022-09-09 21:35:43 +0100

    Renamed main header file

--- include/cutility.h
+++ include/cutility.h
@@ -1,5 +1,5 @@
 /**
- * @file all.h
+ * @file cutility.h
  * @author Alexandre Boavida (novaisboavida@gmail.com)
  * @brief main header file for library, encapsulates all other header files
  * @date 2022-09-09
